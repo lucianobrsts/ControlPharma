2ff9c79dc7b79e40b93fd584395ae6bf768545be
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-15 10:26:30 -0300

    Criada Classe dominio Produto e gerada sua tabela MYSQL

---
+++ src/br/com/controlpharma/domain/Produto.java
@@ -0,0 +1,77 @@
+package br.com.controlpharma.domain;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "idProduto")
+	private Long idProduto;
+
+	@Column(name = "descricao", length = 50, nullable = false)
+	private String descricao;
+
+	@Column(name = "preco", precision = 7, scale = 2, nullable = false)
+	private BigDecimal preco;
+
+	@Column(name = "quantidade", nullable = false)
+	private Integer quantidade;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "fabricante_idFabricante", referencedColumnName = "idFabricante")
+	private Fabricante fabricante;
+
+	public Long getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Long idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+}
