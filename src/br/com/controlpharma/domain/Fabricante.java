85c2bb728ae55fb82452b1127aca97625f9ea067
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-15 00:30:02 -0300

    Criada Classe Dominio Fabricante e gerada sua tabela MySQL

---
+++ src/br/com/controlpharma/domain/Fabricante.java
@@ -0,0 +1,38 @@
+package br.com.controlpharma.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fabricante")
+public class Fabricante {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "idFabricante")
+	private Long idFabricante;
+
+	@Column(name = "descricao", length = 50, nullable = false)
+	private String descricao;
+
+	public Long getIdFabricante() {
+		return idFabricante;
+	}
+
+	public void setIdFabricante(Long idFabricante) {
+		this.idFabricante = idFabricante;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
