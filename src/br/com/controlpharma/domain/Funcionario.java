4315baa3cf02def3f99c8e20f249fbb645f674bb
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-15 09:58:28 -0300

    Criada Classe dominio Funcionario e gerada sua tabela MYSQL

--- src/br/com/controlpharma/domain/Funcionario.java
+++ src/br/com/controlpharma/domain/Funcionario.java
@@ -1,5 +1,71 @@
 package br.com.controlpharma.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "funcionario")
 public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "idFuncionario")
+	private Long idFuncionario;
+
+	@Column(name = "nome", length = 50, nullable = false)
+	private String nome;
+
+	@Column(name = "cpf", length = 14, nullable = false, unique = true)
+	private String cpf;
+
+	@Column(name = "senha", length = 32, nullable = false)
+	private String senha;
+
+	@Column(name = "funcao", length = 50, nullable = false)
+	private String funcao;
+
+	public Long getIdFuncionario() {
+		return idFuncionario;
+	}
+
+	public void setIdFuncionario(Long idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
 
 }
