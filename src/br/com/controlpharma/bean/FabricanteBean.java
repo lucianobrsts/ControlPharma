f29c41212b2b4d320a6ef2d162fd08064c8f4780
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-23 14:28:51 -0300

    Adicionado modelo do fabricante e do funcionario amarrado aos seus xhtml

--- src/br/com/controlpharma/bean/FabricanteBean.java
+++ src/br/com/controlpharma/bean/FabricanteBean.java
@@ -3,12 +3,27 @@ package br.com.controlpharma.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.controlpharma.domain.Fabricante;
 import br.com.controlpharma.util.FacesUtil;
 
 @ManagedBean
 @ViewScoped
 public class FabricanteBean {
+	private Fabricante fabricanteCadastro;
+
+	public Fabricante getFabricanteCadastro() {
+		if (fabricanteCadastro == null) {
+			fabricanteCadastro = new Fabricante();
+		}
+		return fabricanteCadastro;
+	}
+
+	public void setFabricanteCadastro(Fabricante fabricanteCadastro) {
+		this.fabricanteCadastro = fabricanteCadastro;
+	}
+
 	public void salvar() {
+
 		FacesUtil.adicionarMensagemInfo("Fabricante salvo com sucesso!");
 	}
 }
--- src/br/com/controlpharma/bean/FuncionarioBean.java
+++ src/br/com/controlpharma/bean/FuncionarioBean.java
@@ -3,12 +3,27 @@ package br.com.controlpharma.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.controlpharma.domain.Funcionario;
 import br.com.controlpharma.util.FacesUtil;
 
 @ManagedBean
 @ViewScoped
 public class FuncionarioBean {
+	private Funcionario funcionarioCadastro;
+
+	public Funcionario getFuncionarioCadastro() {
+		if (funcionarioCadastro == null) {
+			funcionarioCadastro = new Funcionario();
+		}
+		return funcionarioCadastro;
+	}
+
+	public void setFuncionarioCadastro(Funcionario funcionarioCadastro) {
+		this.funcionarioCadastro = funcionarioCadastro;
+	}
+
 	public void salvar() {
+		FacesUtil.adicionarMensagemInfo(funcionarioCadastro.toString());
 		FacesUtil.adicionarMensagemInfo("Funcionário salvo com sucesso!");
 	}
 }
