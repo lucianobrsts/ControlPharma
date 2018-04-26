d05598237cb64e6493a64ce62725f8e1519eb6d5
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-26 16:30:56 -0300

    Acrescentado anotações Hibernation Validation

--- src/br/com/controlpharma/domain/Funcionario.java
+++ src/br/com/controlpharma/domain/Funcionario.java
@@ -8,7 +8,12 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.br.CPF;
+
+@SuppressWarnings("deprecation")
 @Entity
 @Table(name = "funcionario")
 @NamedQueries({ @NamedQuery(name = "Funcionario.listar", query = "SELECT funcionario FROM Funcionario funcionario"),
@@ -20,15 +25,21 @@ public class Funcionario {
 	@Column(name = "idFuncionario")
 	private Long idFuncionario;
 
+	@NotEmpty(message = "O campo Nome é obrigatório.")
+	@Size(min = 5, max = 50, message = "Tamanho inválido para o campo Nome (5 - 10)")
 	@Column(name = "nome", length = 50, nullable = false)
 	private String nome;
 
+	@CPF(message="O CPF informado é inválido.")
 	@Column(name = "cpf", length = 14, nullable = false, unique = true)
 	private String cpf;
 
+	@NotEmpty(message = "O campo Senha é obrigatório.")
+	@Size(min = 5, max = 50, message = "Tamanho inválido para o campo Senha (6 - 8)")
 	@Column(name = "senha", length = 32, nullable = false)
 	private String senha;
 
+	@NotEmpty(message = "O campo Função é obrigatório.")
 	@Column(name = "funcao", length = 50, nullable = false)
 	private String funcao;
 
