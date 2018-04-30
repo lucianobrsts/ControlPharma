a777dcf250e99ae07716144e739ae1fea5070c6e
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-30 09:23:22 -0300

    Adicionado classes e métodos para autenticação do funcionário

---
+++ src/br/com/controlpharma/bean/AutenticacaoBean.java
@@ -0,0 +1,41 @@
+package br.com.controlpharma.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.controlpharma.dao.FuncionarioDAO;
+import br.com.controlpharma.domain.Funcionario;
+import br.com.controlpharma.util.FacesUtil;
+
+@ManagedBean
+@SessionScoped
+public class AutenticacaoBean {
+	private Funcionario funcionarioLogado;
+
+	public Funcionario getFuncionarioLogado() {
+		if (funcionarioLogado == null) {
+			funcionarioLogado = new Funcionario();
+		}
+		return funcionarioLogado;
+	}
+
+	public void setFuncionarioLogado(Funcionario funcionarioLogado) {
+		this.funcionarioLogado = funcionarioLogado;
+	}
+
+	public void autenticar() {
+		try {
+			FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+			funcionarioLogado = funcionarioDAO.autenticar(funcionarioLogado.getCpf(), funcionarioLogado.getSenha());
+			if (funcionarioLogado == null) {
+				FacesUtil.adiconarMensagemErro("CPF ou senha inválido");
+			} else {
+				FacesUtil.adicionarMensagemInfo("Funcionário autenticado com sucesso.");
+			}
+
+		} catch (RuntimeException ex) {
+			FacesUtil.adiconarMensagemErro("Erro ao tentar autenticar no sistema.");
+		}
+	}
+
+}
