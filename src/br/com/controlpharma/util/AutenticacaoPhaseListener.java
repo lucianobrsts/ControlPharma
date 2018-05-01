d0c016d18bee294624f77db7273ce46855ee295f
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-05-01 09:10:39 -0300

    Finalizado restrinção de navegação via Phase Listener

--- src/br/com/controlpharma/util/AutenticacaoPhaseListener.java
+++ src/br/com/controlpharma/util/AutenticacaoPhaseListener.java
@@ -1,10 +1,18 @@
 package br.com.controlpharma.util;
 
+import java.util.Map;
+
+import javax.faces.application.Application;
+import javax.faces.application.NavigationHandler;
 import javax.faces.component.UIViewRoot;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
+
+import br.com.controlpharma.bean.AutenticacaoBean;
+import br.com.controlpharma.domain.Funcionario;
 
 @SuppressWarnings("serial")
 public class AutenticacaoPhaseListener implements PhaseListener {
@@ -16,7 +24,23 @@ public class AutenticacaoPhaseListener implements PhaseListener {
 		String paginaAtual = uiViewRoot.getViewId();
 
 		boolean ehPaginaAutenticacao = paginaAtual.contains("autenticacao.xhtml");
-		
+
+		if (!ehPaginaAutenticacao) {
+			ExternalContext externalContext = facesContext.getExternalContext();
+			Map<String, Object> mapa = externalContext.getSessionMap();
+			AutenticacaoBean autenticacaoBean = (AutenticacaoBean) mapa.get("autenticacaoBean");
+
+			Funcionario funcionario = autenticacaoBean.getFuncionarioLogado();
+
+			if (funcionario.getFuncao() == null) {
+				FacesUtil.adiconarMensagemErro("Funcionário não autenticado. Faça o Login.");
+
+				Application application = facesContext.getApplication();
+				NavigationHandler navigationHandler = application.getNavigationHandler();
+				navigationHandler.handleNavigation(facesContext, null, "/pages.autenticacao.xhtml?faces-redirect=true");
+			}
+		}
+
 	}
 
 	@Override
