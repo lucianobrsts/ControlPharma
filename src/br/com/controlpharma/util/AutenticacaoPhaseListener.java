8fcec83195ef21f5184511a2889df638857ad223
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-05-01 08:52:12 -0300

    Adicionado filtro de navegação - Phase Listener

---
+++ src/br/com/controlpharma/util/AutenticacaoPhaseListener.java
@@ -0,0 +1,31 @@
+package br.com.controlpharma.util;
+
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+@SuppressWarnings("serial")
+public class AutenticacaoPhaseListener implements PhaseListener {
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		FacesContext facesContext = event.getFacesContext();
+		UIViewRoot uiViewRoot = facesContext.getViewRoot();
+		String paginaAtual = uiViewRoot.getViewId();
+
+		boolean ehPaginaAutenticacao = paginaAtual.contains("autenticacao.xhtml");
+		
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.RESTORE_VIEW;
+	}
+
+}
